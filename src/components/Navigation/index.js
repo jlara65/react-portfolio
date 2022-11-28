8d83e19b842ad7e6fef3b674447b248531138b6a
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-11-28 01:45:43 -0600

    Added profile photo

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -15,7 +15,11 @@ function Navigation(props) {
   return (
     <Nav className="side-nav">
       <div className="container-fluid text-center">
-        <img />
+        <img
+          src="https://i.imgur.com/uCUDEMv.jpg"
+          alt="profile headshot"
+          className="side-nav-img mb-4"
+        />
         <h2 className="nav-title text-center pb-log-3 pb-0">Joseph Lara</h2>
         <ul className="d-flex flex-lg-column flex-row justify-content-center flex-wrap text-center px-0 pt-lg-3 pt-0">
           <a
