880ba0f770f23028016cbdb2f1e221f9e6bcb34e
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-11-30 03:39:56 -0600

    Installed react-animation to use some animation effect

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import { bounceOut } from 'react-animations';
+
+const BounceOut = styled.div`
+  animation: 4s ${keyframes`${bounceOut}`} infinite;
+`;
 
 function Home() {
   return (
     <div className="container-fluid">
-      <h2 className="pe-3 border-end text-end me-5 mb-5">
-        Welcome to my Portfolio
-      </h2>
+      <BounceOut>
+        <h2 className="pe-3 border-end text-end me-5 mb-5">
+          Welcome to my Portfolio
+        </h2>
+      </BounceOut>
     </div>
   );
 }
